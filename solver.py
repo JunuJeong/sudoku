64b68dc580877bddb1f7a2ed65e1815366d82032
Jade Kim <acejadekim@gmail.com>
2019-02-12 10:04:33 +0900

    Solver updated.

--- solver.py
+++ solver.py
@@ -22,6 +22,33 @@ class solver:
 		if cand > 0:
 			print(f'{cand: d} naked singles are evaluated.')
 
+	def solve_hidden_single(self):
+		cand = 0
+		def test_single(blocks):
+			for block in blocks:
+				for i in range(9):
+					if 0 << i in block:
+						continue
+					for 
+
+
+				1 << i
+				map(lambda x, y: x & b, list)
+				for element in block:
+
+
+	def solve_naked_pair(self):
+		cand = 0	
+		virtual_board = self.board.notes.copy()
+		def test_pair(blocks):
+			for row in self.board.notes:
+				for element in row:
+					if bin(element).count('1') != 2
+						continue
+
+
+
+
 
 if __name__ == '__main__':
 	solv = solver('map/base3_3_0.sudoku')
